a8dd015f66c2d861d202dd017c81d4dc286b3618
Usha Sai Chintha <ushasai.chintha@kroger.com>
2022-05-31 07:51:57 +0530

    Usha | Added desired capabilities for an emulator wrt VODQA App

---
+++ src/main/java/EmulatorDesiredCapabilities.java
@@ -0,0 +1,19 @@
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class EmulatorDesiredCapabilities {
+    public static void main(String[] args) throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("appium:deviceName", "APPIUM_EMULATOR");
+        desiredCapabilities.setCapability("appium:app", "/Users/THW3350/AppiumWorkshop/Apps/VodQA.apk");
+        desiredCapabilities.setCapability("appium:automationName", "UiAutomator2");
+        desiredCapabilities.setCapability("appium:packageName", "com.vodqareactnative");
+        desiredCapabilities.setCapability("appium:activityName", "com.vodqareactnative/.MainActivity");
+
+        AndroidDriver androidDriver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+    }
+}
